43e5c3f31c2b96a99a50ec838644e9a2f58d20f8
massn-san <crossarmbar@gmail.com>
2013-02-14 17:43:30 +0900

    Update no_if_fizz_buzz.cpp
    FizzBuzz to fizzbuzz.Delete extra space.

--- no_if_fizz_buzz.cpp
+++ no_if_fizz_buzz.cpp
@@ -6,19 +6,19 @@ int main(void) {
     int fizz = 3;//どの数の倍数でfizzするか
     int buzz = 5;//どの数の倍数でbuzzするか
 
-    int N = 100;//FizzBuzzする範囲
+    int N = 100;//fizzbuzzする範囲
     for(int i = 1 ; i < N ; ++i){
 
         for(int j = 0; j < 1 && j != i%fizz && j != i%buzz; ++j){
-            std::cout << " " << i;
+            std::cout << i;
         }
 
         for(int k = 0; k < 1 - i%fizz; ++k){
-            std::cout << " fizz! ";
+            std::cout << "fizz! ";
         }
 
         for(int l = 0; l < 1 - i%buzz; ++l){
-            std::cout << " buzz! ";
+            std::cout << "buzz!";
         }
 
         std::cout << std::endl;
